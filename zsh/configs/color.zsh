7f8d459e270b047308d6acfc649ebc6a8facf36f
Lee Pender <lpender@gmail.com>
2016-07-15 23:09:13 -0400

    Readable color schema with dark background

--- zsh/configs/color.zsh
+++ zsh/configs/color.zsh
@@ -2,5 +2,8 @@
 autoload -U colors
 colors
 
+# http://geoff.greer.fm/lscolors/
+export LSCOLORS=Cxfxcxdxbxegedabagacad
+
 # enable colored output from ls, etc. on FreeBSD-based systems
 export CLICOLOR=1
